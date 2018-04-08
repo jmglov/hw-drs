18ea84dd7e95a1786c968b135245eaf9f344668b
Joshua Glover <joshua.glover@klarna.com>
2018-04-08 17:28:46 +0200

    Adds publish-aggregates

--- dev/user.clj
+++ dev/user.clj
@@ -5,6 +5,7 @@
             [cheshire.core :as json]
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
+            [clojure.string :as string]
             [hw-drs.config :as config]
             [hw-drs.core :as core]
             [hw-drs.models :as models])
@@ -24,7 +25,7 @@
                                     (ZonedDateTime/of year month day hour minute sec 0 (ZoneOffset/UTC))))
                          (s/gen (s/cat :year (s/int-in 0 2019)
                                        :month (s/int-in 1 13)
-                                       :day (s/int-in 1 28)
+                                       :day (s/int-in 1 29)
                                        :hour (s/int-in 0 24)
                                        :minute (s/int-in 0 60)
                                        :sec (s/int-in 0 60)))))
@@ -34,6 +35,35 @@
                              :purchase/amount (constantly (s/gen (s/int-in 1 50000)))
                              :purchase/merchantId (constantly (gen/fmap str (gen/uuid)))}))
 
+(def report-generators {:hw-drw-report-hour-amount [(s/gen (s/int-in 0 24))
+                                                    (s/gen (s/int-in 1 50000))]
+                        :hw-drw-report-hour-amount-pm [(s/gen (s/int-in 0 24))
+                                                       (s/gen (s/int-in 1 50000))
+                                                       (s/gen :purchase/paymentMethod)]
+                        :hw-drw-report-amount-pm [(s/gen (s/int-in 1 50000))
+                                                  (s/gen :purchase/paymentMethod)]
+                        :hw-drw-report-day-merchant [(s/gen (s/int-in 1 29))
+                                                     (gen/fmap str (gen/uuid))]
+                        :hw-drw-report-merchant-pm [(gen/fmap str (gen/uuid))
+                                                    (s/gen :purchase/paymentMethod)]})
+
+(def datapoint-generator (gen/fmap (fn [report-type]
+                                     (->> (report-generators report-type)
+                                          (map gen/generate)
+                                          (string/join "|")))
+                                   (s/gen #{:hw-drw-report-hour-amount
+                                            :hw-drw-report-hour-amount-pm
+                                            :hw-drw-report-amount-pm
+                                            :hw-drw-report-day-merchant
+                                            :hw-drw-report-merchant-pm})))
+
+(def aggregates-generator (s/gen :aggregate/aggregates
+                                 {:aggregate/datapoint (constantly datapoint-generator)}))
+
 (defn publish-event []
   (sns/publish :topic-arn config/sns-purchase-events
                :message (json/generate-string (gen/generate event-generator))))
+
+(defn publish-aggregates []
+  (sns/publish :topic-arn config/sns-aggregates
+               :message (json/generate-string {:aggregates (gen/generate aggregates-generator)})))
--- src/hw_drs/config.clj
+++ src/hw_drs/config.clj
@@ -6,4 +6,7 @@
 (def sqs-report-day-merchant "https://sqs.eu-west-1.amazonaws.com/166399666252/hw-drw-report-day-merchant")
 (def sqs-report-merchant-pm "https://sqs.eu-west-1.amazonaws.com/166399666252/hw-drw-report-merchant-pm")
 
+(def sqs-aggregates "https://sqs.eu-west-1.amazonaws.com/166399666252/hw-drw-aggregates")
+
 (def sns-purchase-events "arn:aws:sns:eu-west-1:166399666252:hw-drw-purchase-events")
+(def sns-aggregates "arn:aws:sns:eu-west-1:166399666252:hw-drw-aggregates")
