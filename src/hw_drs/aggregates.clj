2e8112ddcedc76e780c87d61e8b89271432514cd
Joshua Glover <joshua.glover@klarna.com>
2018-04-09 08:51:36 +0200

    Adds aggregates/event->aggregate

--- src/hw_drs/aggregates.clj
+++ src/hw_drs/aggregates.clj
@@ -2,15 +2,23 @@
   (:require [hw-drs.core :as core]
             [clojure.string :as string]))
 
+(defn amount->bucket [amount]
+  (cond
+    (< amount 10) "<10"
+    (and (>= amount 10) (< amount 50)) "10-50"
+    (and (>= amount 50) (< amount 100)) "50-100"
+    (and (>= amount 100) (< amount 500)) "100-500"
+    :default ">500"))
+
 (def aggregators {:hour-amount (fn [{:keys [date amount]}]
                                  (string/join "|" [(-> date core/parse-ts core/render-hours)
-                                                   amount]))
+                                                   (amount->bucket amount)]))
                   :hour-amount-pm (fn [{:keys [date amount paymentMethod]}]
                                     (string/join "|" [(-> date core/parse-ts core/render-hours)
-                                                      amount
+                                                      (amount->bucket amount)
                                                       paymentMethod]))
                   :amount-pm (fn [{:keys [amount paymentMethod]}]
-                               (string/join "|" [amount
+                               (string/join "|" [(amount->bucket amount)
                                                  paymentMethod]))
                   :day-merchant (fn [{:keys [date merchantId]}]
                                   (string/join "|" [(-> date core/parse-ts core/render-days)
@@ -19,5 +27,13 @@
                                  (string/join "|" [merchantId
                                                    paymentMethod]))})
 
-(defn event->aggregate [event report-type]
+(defn event->datapoint [event report-type]
   ((aggregators report-type) event))
+
+(defn events->aggregates [events report-type]
+  {:aggregates
+   (->> events
+        (reduce (fn [agg event]
+                  (update agg (event->datapoint event report-type) #((fnil inc 0) %)))
+                {})
+        (map (fn [[datapoint events]] {:datapoint datapoint, :events events})))})
